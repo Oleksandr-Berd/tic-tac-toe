5613b85f9e6720d9d483327c4fa2d406280e583b
Oleksandr-Berd <alex.berd86@gmail.com>
2023-08-05 20:03:24 +0300

    start menu is done

--- src/components/SharedLayout/SharedLayoutStyled.ts
+++ src/components/SharedLayout/SharedLayoutStyled.ts
@@ -1,4 +1,11 @@
 import styled from "styled-components";
 
 export const SharedLayoutStyled = styled.div`
-text-align:center`
+  padding-right: ${(props) => props.theme.space[6]};
+  padding-left: ${(props) => props.theme.space[6]};
+  padding-top: 119px;
+  padding-bottom: 119px;
+
+  text-align: center;
+`;
+
