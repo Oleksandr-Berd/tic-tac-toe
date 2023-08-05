8eee4b7a8deb670a061a454159b229a7f2e3b861
Oleksandr-Berd <alex.berd86@gmail.com>
2023-08-06 01:25:31 +0300

    check on victory

--- src/components/PlayBoard/PlayBoard.tsx
+++ src/components/PlayBoard/PlayBoard.tsx
@@ -9,19 +9,42 @@ interface IProps {
 }
 
 const PlayBoard: React.FC<IProps> = ({ click, currentPlayer }): JSX.Element => {
-    const handleClick = () => {
+    const [xArray, setXArray] = React.useState<number[]>([])
+    const [oArray, setOArray] = React.useState<number[]>([])
+
+
+const winningCombinations = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8], 
+        [0, 3, 6], [1, 4, 7], [2, 5, 8], 
+        [0, 4, 8], [2, 4, 6]            
+    ];
+
+    const handleClick = (index:number) => {
         click();
+        if (currentPlayer === "x") {
+            setXArray(prev => [...prev, index])
+        } else {
+            setOArray(prev => [...prev, index])
+        }
     };
+    
+    const testX = winningCombinations.map((el) => el.every(subel => xArray.includes(subel)))
+    const testO = winningCombinations.map((el) => el.every(subel => oArray.includes(subel)))
+
+   
+
+    if (testX.includes(true)) alert("victory X")
+    if (testO.includes(true)) alert("victory Y")
+
 
     const elementsArray = new Array(9).fill("");
-
-    console.log(elementsArray);
 
     return (
         <SC.GridContainer>
             {elementsArray.map((el, idx) => (
                 <PlayBoardItem
                     key={idx}
+                    index={idx}
                     moveClick={handleClick}
                     currentPlayer={currentPlayer}
                 />
--- src/components/PlayBoard/PlayBoardItem.tsx
+++ src/components/PlayBoard/PlayBoardItem.tsx
@@ -5,24 +5,27 @@ import { ReactComponent as OSvg } from "../../assets/icon-o.svg";
 import * as SC from "./PlayBoardStyled"
 
 interface IProps {
-    moveClick: () => void;
+    moveClick: (index:number) => void;
     currentPlayer: string;
+    index:number,
 }
 
-const PlayBoardItem: React.FC<IProps> = ({ moveClick, currentPlayer }): JSX.Element => {
+const PlayBoardItem: React.FC<IProps> = ({ moveClick, currentPlayer, index }): JSX.Element => {
     const [isClicked, setIsClicked] = React.useState<boolean>(false)
-    const [isMoved , setIsMoved] = React.useState("")
+    const [isMoved, setIsMoved] = React.useState("")
+
+
 
     const handleClick = (evt: any) => {
-        
-        
         setIsMoved(currentPlayer)
-        moveClick()
-    setIsClicked(!isClicked)
-}
+        moveClick(index)
+       
+        setIsClicked(!isClicked)
+    }       
 
+    
     return (
-        <SC.Item onClick={handleClick}> {/* Corrected the prop name */}
+        <SC.Item onClick={handleClick}>
             {isClicked && isMoved === "x" ? <XSvg fill="#31C3BD" width={40} /> : isClicked && isMoved === "o" ? <OSvg fill="#F2B137" width={40}/> : null}
         </SC.Item>
     );
